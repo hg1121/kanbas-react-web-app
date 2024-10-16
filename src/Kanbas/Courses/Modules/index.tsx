2831ae8d9176834b510dda10ad6efd68f5d5ffaa
“HongGuo” <guohong770@gmail.com>
2024-10-15 20:05:02 -0700

    add more data

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -26,14 +26,14 @@ export default function Modules({ cid }: ModulesProps) {
       {modules
           .filter((module: any) => module.course === courseId)
           .map((module: any, index: any) => (
-          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray" key={index}>
+          <li className=" list-group-item p-0 mb-5 fs-5 border-gray" key={index}>
             <div className="wd-title p-3 ps-2 bg-secondary">
               <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
             </div>
             {module.lessons && (
               <ul className="wd-lessons list-group rounded-0">
                 {module.lessons.map((lesson: any) => (
-                  <li className="wd-lesson list-group-item p-3 ps-1">
+                  <li className="wd-module wd-lesson list-group-item p-3 ps-1">
                     <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
                   </li>
                 ))}
