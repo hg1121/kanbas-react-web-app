40b9f7655c90ee7f79a9bb069d08f202e22fb3c6
“HongGuo” <guohong770@gmail.com>
2024-11-22 13:12:13 -0800

    add server link in TOC

--- src/T0C.tsx
+++ src/T0C.tsx
@@ -9,6 +9,8 @@ export default function TOC() {
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
         <li><Link to='https://github.com/hg1121/kanbas-react-web-app/tree/main' id='wd-github'>GitHub</Link></li>
+        <li><Link to="https://kanbas-node-server-app-1-n0tk.onrender.com">Server on Render</Link></li>
+        <li><Link to="https://github.com/hg1121/kanbas-node-server-app">Server GitHub</Link></li>
       </ul>
     );
   }
